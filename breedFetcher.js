d9e098f481c550d163dc0530fabc1c45bb222ee9
marcschul <marcschulm@gmail.com>
2021-08-18 21:53:02 +0000

    refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,19 @@
 const request = require('request');
-const input = process.argv.slice(2);
-const url = 'https://api.thecatapi.cm/v1/breeds/search?q=' + input;
 
-request(url, (error, response, body) => {
-  if (error !== null) {
-    console.log('error: ', error);
-    return;
-  }
+const fetchBreedDescription = function(breedName, callback) {
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data[0] === undefined) {
+      callback('Breed is not found');
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+};
 
-  const data = JSON.parse(body);
-
-  if (data[0] === undefined) {
-    console.log('Breed is not found');
-  } else {
-    console.log(data[0].description);
-  }
-});
+module.exports = { fetchBreedDescription };
